0b2da526f265c87464bacabfc2a57e271ce08917
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-04-03 13:10:46 +0200

    I've added the devise gem, done the installation process and configured it a lot of stuff regarding it. There are pending stuff like adding to the User model a table for first name, last name, telegram user... On the other hand, also included a navbar and the flash messages for wrong information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
 end
