f6ba38db3a71a3d440745ff2ed2c46ff4ee73777
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-04-03 16:54:21 +0200

    DB schema completed. Now pending to do the seeding.

---
+++ db/migrate/20230403143004_create_bungalows.rb
@@ -0,0 +1,11 @@
+class CreateBungalows < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bungalows do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_112330) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_144656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bungalows", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bungalows_on_user_id"
+  end
+
+  create_table "challenges", force: :cascade do |t|
+    t.text "description"
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "challenges_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "challenge_id", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_112330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bungalows", "users"
 end
