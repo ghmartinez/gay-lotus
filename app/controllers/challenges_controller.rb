4d79e15f25d150470b77e92c68d52fad6c90c2aa
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-04-11 19:58:56 +0200

    tengo que seguir trabajando

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,28 +1,2 @@
 class ChallengesController < ApplicationController
-  def index
-    @challenges = current_user.challenges
-  end
-
-  def show
-    @challenge = Challenge.find(params[:id])
-  end
-
-  # def new
-  #   @challenge = Challenge.new
-  # end
-
-  # def create
-  #   @challenge = Challenge.new(challenge_params)
-  #   if @challenge.save
-  #     redirect_to @challenge
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # private
-
-  # def challenge_params
-  #   params.require(:challenge).permit(:name, :description, :start_date, :end_date)
-  # end
 end
