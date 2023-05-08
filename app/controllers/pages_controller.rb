91c45b0715816dccdc13a2f76852a97b06d8f736
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-05-08 16:10:36 +0200

    logic in theory regarding qrs and missions, well implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,15 +5,13 @@ class PagesController < ApplicationController
   end
 
   def scan
-    # Si el usuario no tiene qr, se lo asigno.
+    # Al registrarse / iniciar sesión desde el qr, al usuario se le asigna un qr.
     if current_user.qr.nil?
       current_user.update(qr: params[:qr])
     # Si el usuario tiene un qr diferent al suyo y el qr está presente en el enlace:
     elsif current_user.qr != params[:qr] && params[:qr].present?
       # Actualizo el estado y el ganador de la misión del current user.
-      current_user.missions.last.status = "completed"
-      current_user.missions.last.winner_user = current_user
-      current_user.missions.last.save
+      current_user.missions.last.update(status: "completed", winner_user: current_user)
 
       # Encuentro al usuario que tiene asignado el qr que se ha escaneado.
       @user = User.find_by(qr: params[:qr])
@@ -23,15 +21,17 @@ class PagesController < ApplicationController
         @mensaje = "El qr que has escaneado no está asociado a ningún usuario."
 
       # Compruebo que
+      elsif current_user.missions.any? do |mission|
+        mission.user.qr == params[:qr]
+        @mensaje = "Ya has escaneado este qr de esta nueva mision, tonta."
+      end
+
       elsif current_user.missions.last.status == "completed"
         @mission = Mission.find_by(user: @user)
         @new_mission = @mission.dup
-        @new_mission.user = current_user
-        @new_mission.save
-      else
-        @mensaje = "ya has escaneado este collar, sigue con tu mision hija mia del amor hermoso, tonta."
+        @new_mission.update(user: current_user)
       end
-    else
+    elsif current_user.qr == params[:qr]
       @mensaje = "Ya has registrado tu propio qr, tonta."
     end
   end
