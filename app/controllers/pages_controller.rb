fd7eaaed4f39344ec3b448db8cc1300b0c37b5df
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-05-08 11:49:18 +0200

    some changes to the qr and missions logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     # Si el usuario no tiene qr, se lo asigno.
     if current_user.qr.nil?
       current_user.update(qr: params[:qr])
-    # Si el usuario tiene qr y el qr que se ha escaneado es el suyo, no hago nada.
+    # Si el usuario tiene un qr diferent al suyo y el qr está presente en el enlace:
     elsif current_user.qr != params[:qr] && params[:qr].present?
       # Actualizo el estado y el ganador de la misión del current user.
       current_user.missions.last.status = "completed"
@@ -17,22 +17,22 @@ class PagesController < ApplicationController
 
       # Encuentro al usuario que tiene asignado el qr que se ha escaneado.
       @user = User.find_by(qr: params[:qr])
+
+      # Compruebo que el qr esté asociado a alguien.
       if @user.nil?
-        @amor = "El qr que has escaneado no está asociado a ningún usuario."
-      else
+        @mensaje = "El qr que has escaneado no está asociado a ningún usuario."
+
+      # Compruebo que
+      elsif current_user.missions.last.status == "completed"
         @mission = Mission.find_by(user: @user)
-        current_user.missions.each do |mission|
-          if mission.challenge == @mission.challenge && mission.target_user == @mission.target_user && mission.status == @mission.status
-            @amor = "ya has escaneado este collar, sigue con tu mision hija mia del amor hermoso, tonta."
-          end
-        end
         @new_mission = @mission.dup
         @new_mission.user = current_user
         @new_mission.save
+      else
+        @mensaje = "ya has escaneado este collar, sigue con tu mision hija mia del amor hermoso, tonta."
       end
-      # Encuentro la misión que tiene asignada el usuario que tiene asignado el qr que se ha escaneado y duplico la misión.
     else
-      @amor = "Ya has registrado tu propio qr, tonta."
+      @mensaje = "Ya has registrado tu propio qr, tonta."
     end
   end
 end
