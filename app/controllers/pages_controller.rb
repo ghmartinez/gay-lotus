99c8ff4114a4175e5b7cd70eae36a91eee3e86d1
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-05-08 17:01:29 +0200

    I think that the lofic of QRs and Missions is correct now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,34 +5,29 @@ class PagesController < ApplicationController
   end
 
   def scan
-    # Al registrarse / iniciar sesión desde el qr, al usuario se le asigna un qr.
     if current_user.qr.nil?
       current_user.update(qr: params[:qr])
-    # Si el usuario tiene un qr diferent al suyo y el qr está presente en el enlace:
-    elsif current_user.qr != params[:qr] && params[:qr].present?
-      # Actualizo el estado y el ganador de la misión del current user.
-      current_user.missions.last.update(status: "completed", winner_user: current_user)
 
-      # Encuentro al usuario que tiene asignado el qr que se ha escaneado.
+    elsif current_user.qr != params[:qr] && params[:qr].present?
+
       @user = User.find_by(qr: params[:qr])
 
-      # Compruebo que el qr esté asociado a alguien.
       if @user.nil?
         @mensaje = "El qr que has escaneado no está asociado a ningún usuario."
 
-      # Compruebo que
-      elsif current_user.missions.any? do |mission|
-        mission.user.qr == params[:qr]
-        @mensaje = "Ya has escaneado este qr de esta nueva mision, tonta."
-      end
+      elsif current_user.qr == params[:qr]
+        @mensaje = "Ya has registrado tu propio qr, tonta."
 
-      elsif current_user.missions.last.status == "completed"
+      elsif current_user.missions.any? { |mission| mission.target_user.qr == params[:qr] && mission.status == "completed" }
+        @mensaje = "Ya has escaneado este qr de esta mision, tonta."
+
+      else
+        current_user.missions.last.update(status: "completed", winner_user: current_user)
         @mission = Mission.find_by(user: @user)
         @new_mission = @mission.dup
         @new_mission.update(user: current_user)
+        @mensaje = "Has completado la misión, tonta."
       end
-    elsif current_user.qr == params[:qr]
-      @mensaje = "Ya has registrado tu propio qr, tonta."
     end
   end
 end
