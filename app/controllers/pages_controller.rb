320ffd9bcfc67b34560a161ca19c59510a82ca1c
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-05-11 15:45:14 +0200

    fixing problems, more to do

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,18 +29,32 @@ class PagesController < ApplicationController
       if @user.nil?
         @mensaje = "El qr que has escaneado no está asociado a ningún usuario."
 
+      # condicion que dentro de mis mis misiones, el target escaneado sea diferente a mi mision actual
+      #  mensaje = esta mision no es tuya
+      elsif current_user.missions.none? { |mission| mission.target_user.qr == params[:qr] }
+        @mensaje = "Esta misión no es tuya, tonta."
+
       elsif current_user.missions.any? { |mission| mission.target_user.qr == params[:qr] && mission.status == "completed" }
-        @mensaje = "Ya has escaneado este qr de esta mision, tonta."
+        @mensaje = "Ya has ganado esta mision, tonta."
 
       elsif
         @other_mission.present? && @other_mission.status == "completed"
         @mensaje = "QUE HAS PERDIDO, DEJA DE ESCANEAr ya porfavor"
       else
+        # Actualizar mision actual como completada en todas sus copias
         current_user.missions.last.update(status: "completed", winner_user: current_user)
+        #  aqui buscar la mision del otro pavo / o otros pavos y ponersela como ganada (no poner el winner pq pal recuento es mas facil quitarme las que estan nil por defecto)
+
+        @mensaje = "Has completado la misión!"
+
+        # Obtener siguiente mision
         @mission = Mission.find_by(user: @user)
+
         @new_mission = @mission.dup
         @new_mission.update(user: current_user)
-        @mensaje = "Has completado la misión!"
+
+        # (Escenario contemplado en Notion) Aquí tengo que encontrar la misión donde target user, sea el mismo, y duplicar la mision para las personas que tengan ese target.
+        #  la mision es @mision
       end
     end
   end
