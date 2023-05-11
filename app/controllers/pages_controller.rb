5bf451921d7c0477de8b1d4aa51d28f5b38f99b3
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-05-11 17:40:15 +0200

    logic behind the game done, now please i need to focus in the database part for the seeding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,8 +29,6 @@ class PagesController < ApplicationController
       if @user.nil?
         @mensaje = "El qr que has escaneado no está asociado a ningún usuario."
 
-      # condicion que dentro de mis mis misiones, el target escaneado sea diferente a mi mision actual
-      #  mensaje = esta mision no es tuya
       elsif current_user.missions.none? { |mission| mission.target_user.qr == params[:qr] }
         @mensaje = "Esta misión no es tuya, tonta."
 
@@ -43,18 +41,27 @@ class PagesController < ApplicationController
       else
         # Actualizar mision actual como completada en todas sus copias
         current_user.missions.last.update(status: "completed", winner_user: current_user)
+
         #  aqui buscar la mision del otro pavo / o otros pavos y ponersela como ganada (no poner el winner pq pal recuento es mas facil quitarme las que estan nil por defecto)
 
         @mensaje = "Has completado la misión!"
 
         # Obtener siguiente mision
-        @mission = Mission.find_by(user: @user)
+        @mission = Mission.find_by(user: @user) # misión que ahora vas a hacer
+        @target = @mission.target_user # usuario al que tienes que hacer la mision
+
 
         @new_mission = @mission.dup
         @new_mission.update(user: current_user)
-
         # (Escenario contemplado en Notion) Aquí tengo que encontrar la misión donde target user, sea el mismo, y duplicar la mision para las personas que tengan ese target.
         #  la mision es @mision
+
+        # @misiones_a_duplicar = Mission.all.where(target_user: @target) # misiones que tienen como target al usuario al que le has hecho la mision
+        # @misiones_a_duplicar.each do |mision|
+        #   user = mision.user
+        #   raise
+        #   user.missions.create(challenge: mision.challenge, target_user: mision.target_user, status: "pending")
+        # end
       end
     end
   end
